d3cdfd06801572c8b39ac94bf64880f302fb32a0
odvancee <wispofkind@gmail.com>
2026-07-12 15:14:26 +0700

    Update ClearConsole: removed var keywords

--- ClearConsole/ClearConsole.cs
+++ ClearConsole/ClearConsole.cs
@@ -2,6 +2,7 @@
 // Source: https://github.com/odvancee/unity-tools
 
 #if UNITY_EDITOR
+using System;
 using System.Reflection;
 using UnityEditor;
 using UnityEditor.ShortcutManagement;
@@ -12,9 +13,9 @@ public static class ClearConsole
     [Shortcut("Clear Console", KeyCode.C, ShortcutModifiers.Control | ShortcutModifiers.Alt | ShortcutModifiers.Shift)]
     public static void Clear()
     {
-        var assembly = Assembly.GetAssembly(typeof(SceneView));
-        var type = assembly.GetType("UnityEditor.LogEntries");
-        var method = type.GetMethod("Clear");
+        Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+        Type type = assembly.GetType("UnityEditor.LogEntries");
+        MethodInfo method = type.GetMethod("Clear");
         method.Invoke(new object(), null);
     }
 }
